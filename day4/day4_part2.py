0c86c9abde944902775de8f4af9a478c046cbf7c
Gabriella Rydenfors <Gabriella.Rydenfors@gmail.com>
2024-02-01 10:32:04 +0100

    Day 4 part 2, simplified

--- day4/day4_part2.py
+++ day4/day4_part2.py
@@ -16,18 +16,13 @@ def winning_numbers(input_string):
     return winningNumbers
 
 def test_winning_numbers():
-    result = winning_numbers("Card   1: 66 92  4 54 39 76 49 27 61 56 | 66 59 85 54 61 86 37 49  6 18 81 39  4 56  2 48 76 72 71 25 27 67 10 92 13")
-    assert len(result) == 10
-    assert result[0] == 66
-    assert result[1] == 92
-    assert result[2] == 4
-    assert result[3] == 54
-    assert result[4] == 39
-    assert result[5] == 76
-    assert result[6] == 49
-    assert result[7] == 27
-    assert result[8] == 61
-    assert result[9] == 56
+    result = winning_numbers("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+    assert len(result) == 5
+    assert result[0] == 41
+    assert result[1] == 48
+    assert result[2] == 83
+    assert result[3] == 86
+    assert result[4] == 17
 
 def my_numbers(input_string):
 
@@ -70,89 +65,63 @@ def look_for_matches(winningNumbers, myNumbers, copies, nr, lines):
     if matches > 0:
         for i in range(matches):
             if(nr + i + 1 < lines):
-                copies[nr + i + 1].append(myNumbers)
+                copies[nr + i + 1] += copies[nr]
 
 def test_look_for_matches():
     winning_numbers = [41, 48, 83, 86, 17]
     my_numbers = [83, 86, 6, 31, 17, 9, 48, 53]
-    copies = [[] for _ in range(6)]
+    copies = [1 for _ in range(6)]
     look_for_matches(winning_numbers, my_numbers, copies, 0, 6)
-    assert len(copies[0]) == 0
-    assert len(copies[1]) == 1
-    assert copies[1][0] == my_numbers
-    assert len(copies[2]) == 1
-    assert copies[2][0] == my_numbers
-    assert len(copies[3]) == 1
-    assert copies[3][0] == my_numbers
-    assert len(copies[4]) == 1
-    assert copies[4][0] == my_numbers
-    assert len(copies[5]) == 0
+    assert copies[0] == 1
+    assert copies[1] == 2
+    assert copies[2] == 2
+    assert copies[3] == 2
+    assert copies[4] == 2
+    assert copies[5] == 1
 
     winning_numbers = [13, 32, 20, 16, 61]
     my_numbers = [61, 30, 68, 82, 17, 32, 24, 19]    
     look_for_matches(winning_numbers, my_numbers, copies, 1, 6)
-    assert len(copies[0]) == 0
-    assert len(copies[1]) == 1
-    assert len(copies[2]) == 2
-    assert copies[2][1] == my_numbers
-    assert len(copies[3]) == 2
-    assert copies[3][1] == my_numbers
-    assert len(copies[4]) == 1
-    assert len(copies[5]) == 0
+    assert copies[0] == 1
+    assert copies[1] == 2
+    assert copies[2] == 4
+    assert copies[3] == 4
+    assert copies[4] == 2
+    assert copies[5] == 1
 
 
 def test_input_day4():
     with open('day4/test_input_day4.txt', 'r') as file:
-        sum = 0
-        nr = 0
-
+        
         lines = file.readlines()
         assert len(lines) == 6
+        copies = [1 for _ in range(len(lines))]
 
-        copies = [[] for _ in range(len(lines))]
-
-        for card in lines:
-
+        for nr, card in enumerate(lines):
             winningNumbers = winning_numbers(card)
             myNumbers = my_numbers(card)
-
             print("Card " + str(nr) + ": " + str(winningNumbers) + " | " + str(myNumbers))
-
             look_for_matches(winningNumbers, myNumbers, copies, nr, len(lines))
-            sum += 1
-                
-            for myCopiedNumbers in copies[nr]:
-                look_for_matches(winningNumbers, myNumbers, copies, nr, len(lines))
-                sum += 1
-
-            nr += 1
-
+            
+        sum = 0
         for i, copy in enumerate(copies):
-            print("card ", i , ": ", len(copy) + 1)
+            print("card ", i , ": ", copy)
+            sum += copy
         
-        print("copies:", copies)
-
         assert sum == 30
 
 with open('day4/input_day4.txt', 'r') as file:
-    sum = 0
-    nr = 0
 
     lines = file.readlines()
-    copies = [[] for _ in range(len(lines))]
+    copies = [1 for _ in range(len(lines))]
 
-    for card in lines:
-
+    for nr, card in enumerate(lines):
         winningNumbers = winning_numbers(card)
         myNumbers = my_numbers(card)
-
-        look_for_matches(winningNumbers, myNumbers, copies, nr, len(lines))
-        sum += 1
-            
-        for myCopiedNumbers in copies[nr]:
-            look_for_matches(winningNumbers, myNumbers, copies, nr, len(lines))
-            sum += 1
-
-        nr += 1    
+        look_for_matches(winningNumbers, myNumbers, copies, nr, len(lines))            
+    
+    sum = 0
+    for i, copy in enumerate(copies):
+        sum += copy
     
     print("sum:", sum)
