d7530e1885f0dfd93f4950feca10fca3fcfb3650
Gabriella Rydenfors <Gabriella.Rydenfors@gmail.com>
2024-02-05 17:02:43 +0100

    Day 5, part 2: Seeds stored in groupes instead of a long list

--- day5/day5_part2.py
+++ day5/day5_part2.py
@@ -5,57 +5,106 @@ def read_seeds(input_string):
     seeds = []
     input = input_string.split(':')
     start = 0
-    for seed in input[1].strip().split(' '):
+    for nr in input[1].strip().split(' '):
         if start == 0:
-            start = int(seed)
+            start = int(nr)
         else:
-            for i in range(start, start + int(seed)):
-                seeds.append(i)
+            seeds.append((start, int(nr)))
             start = 0
     return seeds
 
 def test_read_seeds():
     input = "seeds: 79 14 55 13"
-    assert read_seeds(input) == [79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67]
+    output = read_seeds(input)
+    assert output == [(79, 14), (55, 13)]
+    assert output[0] == (79, 14)
+    assert output[1] == (55, 13)
+    assert output[0][0] == 79
+    assert output[0][1] == 14
+    assert output[1][0] == 55
+    assert output[1][1] == 13
 
-def convert_nr(seed, convert_string):     
+def convert_nr(seeds, index, convert_string):
+    seedStart, seedLenght = seeds[index]
     destinationStart, sourceStart, rangeLength = map(int, convert_string.split())
-    if seed >= sourceStart and seed < sourceStart + rangeLength:
-        seed = destinationStart + (seed - sourceStart)
-    return seed
+
+    # If seed ranges start before the conversions source range and first part of the seed will not convert
+    if seedStart < sourceStart and seedStart + seedLenght > sourceStart:
+        firstPartSeedLenght = sourceStart - seedStart
+        seeds.append((seedStart, firstPartSeedLenght))
+
+        # Remaining seed that should be converted
+        seedStart = sourceStart
+        seedLenght = seedLenght - firstPartSeedLenght
+
+    if seedStart >= sourceStart and seedStart < sourceStart + rangeLength:
+
+        # If seed ranges end after the conversions source range and last part of the seed will not convert
+        if seedStart + seedLenght > sourceStart + rangeLength:
+            lastPartSeedLenght = (seedStart + seedLenght) - (sourceStart + rangeLength)
+            seeds.append((sourceStart + rangeLength, lastPartSeedLenght))
+            seedLenght = seedLenght - lastPartSeedLenght
+
+        # conversion
+        seedStart = destinationStart + (seedStart - sourceStart)
+        seeds[index] = (seedStart, seedLenght)
+
+    return seeds
 
 def test_convert_nr():
+    seeds = [(79, 14), (55, 13)]
+    assert len(seeds) == 2
+
     input = "50 98 2"
-    assert convert_nr(97, input) == 97
-    assert convert_nr(98, input) == 50
-    assert convert_nr(99, input) == 51
-    assert convert_nr(100, input) == 100
+    for i, seed in enumerate(seeds):
+        seeds = convert_nr(seeds, i, input)
+    assert len(seeds) == 2
+    assert seeds == [(79, 14), (55, 13)]
 
     input = "52 50 48"
-    assert convert_nr(49, input) == 49
-    assert convert_nr(50, input) == 52
-    assert convert_nr(51, input) == 53
-    assert convert_nr(52, input) == 54
-    assert convert_nr(96, input) == 98
-    assert convert_nr(97, input) == 99
-    assert convert_nr(98, input) == 98
+    for i in range(len(seeds)):
+        seeds = convert_nr(seeds, i, input)
+    assert len(seeds) == 2
+    assert seeds == [(81, 14), (57, 13)]
 
-def convert_seeds(seeds, convert_strings):
+    # Test where seed range exceeds the conversion range
+    input = "20 80 4"
+    for i in range(len(seeds)):
+        print("Seed nr ", i, " : ", seeds[i])
+        seeds = convert_nr(seeds, i, input)
+    assert len(seeds) == 3
+    assert seeds == [(21, 3), (57, 13), (84, 11)]
+
+    print(" ---- ")
+
+    # Test where seed range starts before conversion range
+    input = "200 90 20"
+    for i, seed in enumerate(seeds):
+        print("Seed nr ", i, " : ", seeds[i])
+        seeds = convert_nr(seeds, i, input)
+    assert len(seeds) == 4
+    assert seeds[0] == (21, 3)
+    assert seeds[1] == (57, 13)
+    assert seeds[2] == (200, 5)
+    assert seeds[3] == (84, 6)
+    assert seeds == [(21, 3), (57, 13), (200, 5), (84, 6)]
+
+
+def convert_seeds(seeds, convert_strings):    
     for i, seed in enumerate(seeds):
         for convert_string in convert_strings:                    
-            seed = convert_nr(seed, convert_string)
+            seeds = convert_nr(seeds, i, convert_string)
             if seed != seeds[i]: # test against old value to see if seed has been converted
-                # print("Seed coverted from ", seeds[i], " to ", seed)
-                seeds[i] = seed
                 break
-    # print("Seeds: ", seeds)
+        # print("Seed nr ", i, " : ", seed, "convert to: ", seeds[i], " -- seeds: ", seeds)
+            
     return seeds
 
 def test_convert_seeds():
-    seeds = [79, 14, 55, 13]
+    seeds = [(79, 14), (55, 13)]
     convert_strings = ["50 98 2", "52 50 48"]
     seeds = convert_seeds(seeds, convert_strings)
-    assert seeds == [81, 14, 57, 13]
+    assert seeds == [(81, 14), (57, 13)]
 
 def read_and_convert_seeds(lines):
     seeds = []
@@ -75,10 +124,6 @@ def read_and_convert_seeds(lines):
 
     return seeds
 
-# def test_read_and_convert_seeds():
-#     with open('day5/test_input_day5.txt', 'r') as file:
-#         seeds = read_and_convert_seeds(file.readlines())
-#         assert seeds == [82, 43, 86, 35]
 
 def test_day5_part1():
     with open('day5/test_input_day5.txt', 'r') as file:
@@ -88,19 +133,19 @@ def test_day5_part1():
         print('seed: ', seeds)
 
         for seed in seeds:
-            if(seed < lowestLocation):
-                lowestLocation = seed
+            if(seed[0] < lowestLocation):
+                lowestLocation = seed[0]
 
         print("lowestLocation: ", lowestLocation) 
         assert lowestLocation == 46
 
-# # Main Code
-# with open('day5/input_day5.txt', 'r') as file:
-#     seeds = read_and_convert_seeds(file.readlines())
+# Main Code
+with open('day5/input_day5.txt', 'r') as file:
+    seeds = read_and_convert_seeds(file.readlines())
 
-#     lowestLocation = sys.maxsize  
-#     for seed in seeds:
-#         if(seed < lowestLocation):
-#             lowestLocation = seed
+    lowestLocation = sys.maxsize  
+    for seed in seeds:
+        if(seed[0] < lowestLocation):
+            lowestLocation = seed[0]
                 
-#     print("lowestLocation: ", lowestLocation)
+    print("lowestLocation: ", lowestLocation)
