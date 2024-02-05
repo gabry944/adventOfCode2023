351be7750ea848bcf58c8e335906171167839e15
Gabriella Rydenfors <Gabriella.Rydenfors@gmail.com>
2024-02-05 12:00:12 +0100

    Day 5, part 2: Save all Seeds in a list even though it will be quite long

---
+++ day5/day5_part2.py
@@ -0,0 +1,106 @@
+import pytest
+import sys
+
+def read_seeds(input_string):
+    seeds = []
+    input = input_string.split(':')
+    start = 0
+    for seed in input[1].strip().split(' '):
+        if start == 0:
+            start = int(seed)
+        else:
+            for i in range(start, start + int(seed)):
+                seeds.append(i)
+            start = 0
+    return seeds
+
+def test_read_seeds():
+    input = "seeds: 79 14 55 13"
+    assert read_seeds(input) == [79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67]
+
+def convert_nr(seed, convert_string):     
+    destinationStart, sourceStart, rangeLength = map(int, convert_string.split())
+    if seed >= sourceStart and seed < sourceStart + rangeLength:
+        seed = destinationStart + (seed - sourceStart)
+    return seed
+
+def test_convert_nr():
+    input = "50 98 2"
+    assert convert_nr(97, input) == 97
+    assert convert_nr(98, input) == 50
+    assert convert_nr(99, input) == 51
+    assert convert_nr(100, input) == 100
+
+    input = "52 50 48"
+    assert convert_nr(49, input) == 49
+    assert convert_nr(50, input) == 52
+    assert convert_nr(51, input) == 53
+    assert convert_nr(52, input) == 54
+    assert convert_nr(96, input) == 98
+    assert convert_nr(97, input) == 99
+    assert convert_nr(98, input) == 98
+
+def convert_seeds(seeds, convert_strings):
+    for i, seed in enumerate(seeds):
+        for convert_string in convert_strings:                    
+            seed = convert_nr(seed, convert_string)
+            if seed != seeds[i]: # test against old value to see if seed has been converted
+                # print("Seed coverted from ", seeds[i], " to ", seed)
+                seeds[i] = seed
+                break
+    # print("Seeds: ", seeds)
+    return seeds
+
+def test_convert_seeds():
+    seeds = [79, 14, 55, 13]
+    convert_strings = ["50 98 2", "52 50 48"]
+    seeds = convert_seeds(seeds, convert_strings)
+    assert seeds == [81, 14, 57, 13]
+
+def read_and_convert_seeds(lines):
+    seeds = []
+    converters = []         
+
+    for i, line in enumerate(lines):
+        if i == 0:
+            seeds = read_seeds(line.strip())
+        elif len(line.split(':')) == 2:
+            continue
+        elif line == "\n":
+            convert_seeds(seeds, converters)          
+            converters = []
+        else:            
+            converters.append(line.strip())
+    convert_seeds(seeds, converters)
+
+    return seeds
+
+# def test_read_and_convert_seeds():
+#     with open('day5/test_input_day5.txt', 'r') as file:
+#         seeds = read_and_convert_seeds(file.readlines())
+#         assert seeds == [82, 43, 86, 35]
+
+def test_day5_part1():
+    with open('day5/test_input_day5.txt', 'r') as file:
+        seeds = read_and_convert_seeds(file.readlines())
+
+        lowestLocation = sys.maxsize  
+        print('seed: ', seeds)
+
+        for seed in seeds:
+            if(seed < lowestLocation):
+                lowestLocation = seed
+
+        print("lowestLocation: ", lowestLocation) 
+        assert lowestLocation == 46
+
+# # Main Code
+# with open('day5/input_day5.txt', 'r') as file:
+#     seeds = read_and_convert_seeds(file.readlines())
+
+#     lowestLocation = sys.maxsize  
+#     for seed in seeds:
+#         if(seed < lowestLocation):
+#             lowestLocation = seed
+                
+#     print("lowestLocation: ", lowestLocation)
