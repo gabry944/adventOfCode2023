6c0f54be3fd7446b8d3de476178b93f704d1bf0c
Gabriella Rydenfors <Gabriella.Rydenfors@gmail.com>
2024-02-06 10:57:55 +0100

    Day 6, part 1: Determine the number of ways you can beat the record in each boat race

--- day6/day6_part1.py
+++ day6/day6_part1.py
@@ -1,18 +1,82 @@
 import pytest
 import sys
+import re
 
-def read_file(lines):       
+def possible_distances(totalTime):
+    distances = []
+    for holdTime in range(0, totalTime + 1):
+        speed = holdTime
+        travelTime = totalTime - holdTime
+        distances.append(speed * travelTime)
 
-    for i, line in enumerate(lines):
+    return distances
+
+def test_possible_distances():
+    assert possible_distances(7) == [0, 6, 10, 12, 12, 10, 6, 0]
+
+def read_races(lines):
+    times = []
+    distances = []
+
+    for line in lines:
         print("line: ", line.strip())
+        if "Time:" in line:
+            numbers = re.findall(r'\d+', line.strip())
+            for number in numbers:
+                times.append(int(number))
+        if "Distance:" in line:
+            numbers = re.findall(r'\d+', line.strip())
+            for number in numbers:
+                distances.append(int(number))
 
+    return times, distances
+
+def test_read_races():
+    with open('day6/test_input_day6.txt', 'r') as file:
+        time, distance = read_races(file.readlines())
+        assert time == [7, 15, 30]
+        assert distance == [9, 40, 200]
+
+def possible_wins(times, records):
+        possibleWins = []
+        for i, time in enumerate(times):
+            possibleDistances = possible_distances(time)
+            
+            possibleWin = 0
+            for possibleDistance in possibleDistances:
+                if possibleDistance > records[i]:
+                    # print("Possible distance: ", possibleDistance)
+                    possibleWin += 1
+                    
+            possibleWins.append(possibleWin)
+
+        return possibleWins
+
+def test_possible_wins():
+    with open('day6/test_input_day6.txt', 'r') as file:
+        times, records = read_races(file.readlines())
+        possibleWins = possible_wins(times, records)
+        assert possibleWins == [4, 8, 9]
 
 def test_day6_part1():
     with open('day6/test_input_day6.txt', 'r') as file:
-        read_file(file.readlines())
-        assert False
+        times, records = read_races(file.readlines())
+        possibleWins = possible_wins(times, records)
+
+        margin = 1
+        for wins in possibleWins:
+            margin *= wins
+
+        assert margin == 288
 
 
 # Main Code
 with open('day6/input_day6.txt', 'r') as file:
-    read_file(file.readlines())
+        times, records = read_races(file.readlines())
+        possibleWins = possible_wins(times, records)
+
+        margin = 1
+        for wins in possibleWins:
+            margin *= wins
+
+        print("Result: ", margin) 
